72ea044590188a079c5aa84107912c35c989b4ec
rahult-rt <71532679+rahult-rt@users.noreply.github.com>
2021-03-01 07:45:59 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ seconds = date.getSeconds();
 
 month = date.toLocaleString('default', { month: 'short' });
 
-document.getElementById('currentMonth').innerHTML = month +" " +dt;
+document.getElementById('currentMonth').innerHTML = month +" " + dt;
 
 hHandRotation = 30 * hours + minutes/2; //in 60 min 30 deg rotation + (60 min = 30deg so 1 min = 60/30 .i.e. 1/2)
 mHandRotation = 6 * minutes; //in 60 min 360 deg rotation so in 1 min 6 deg (360/60)
